f3db61dc5578427bacca13722222376fac68aee2
sarad <sarad.mohanan@gmail.com>
2022-04-19 17:44:11 +0530

    adding caching

--- src/main/java/com/hevodata/mavens3wagon/HevoS3Wagon.java
+++ src/main/java/com/hevodata/mavens3wagon/HevoS3Wagon.java
@@ -2,11 +2,12 @@ package com.hevodata.mavens3wagon;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kong.unirest.GetRequest;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
-import kong.unirest.json.JSONObject;
 import org.apache.maven.wagon.authentication.AuthenticationInfo;
 import org.apache.maven.wagon.repository.Repository;
 import org.kuali.maven.wagon.S3Wagon;
@@ -14,10 +15,16 @@ import org.kuali.maven.wagon.auth.AwsSessionCredentials;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Map;
+
 public class HevoS3Wagon extends S3Wagon {
 
     private String authUrl;
     private static final Logger log = LoggerFactory.getLogger(HevoS3Wagon.class);
+    private final File credentialsCachePath = new File("/tmp/hevo_s3_wagon");
 
     public void setAuthUrl(String authUrl) {
         this.authUrl = authUrl;
@@ -35,19 +42,36 @@ public class HevoS3Wagon extends S3Wagon {
     @Override
     protected AWSCredentials getCredentials(final AuthenticationInfo authenticationInfo) {
         try {
-            GetRequest getRequest = Unirest.get(this.getAuthUrl());
-            if (authenticationInfo != null && authenticationInfo.getUserName() != null) {
-                getRequest.basicAuth(authenticationInfo.getUserName(), authenticationInfo.getPassword());
+            if(!credentialsCachePath.exists()){
+                getAwsSessionCredentials(authenticationInfo);
             }
-            HttpResponse<JsonNode> response = getRequest.asJson();
-            JSONObject jsonObject = response.getBody().getObject();
-            String accessKey = jsonObject.getString("access_key");
-            String secretKey = jsonObject.getString("secret_key");
-            String sessionToken = jsonObject.getString("token");
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, Object> credentials = objectMapper.readValue(credentialsCachePath, new TypeReference<Map<String,Object>>(){});
+            long expiry = ((Number) credentials.getOrDefault("expiry", 0)).longValue();
+            if ((System.currentTimeMillis() / 1000) > expiry) {
+                getAwsSessionCredentials(authenticationInfo);
+                credentials = objectMapper.readValue(credentialsCachePath, new TypeReference<Map<String,Object>>(){});
+            }
+            String accessKey = (String) credentials.get("access_key");
+            String secretKey = (String) credentials.get("secret_key");
+            String sessionToken = (String) credentials.get("token");
             return new AwsSessionCredentials(accessKey, secretKey, sessionToken);
         } catch (Exception e) {
             log.error(e.getMessage());
-            throw e;
+            return null;
+        }
+    }
+
+    private void getAwsSessionCredentials(final AuthenticationInfo authenticationInfo) throws FileNotFoundException {
+        GetRequest getRequest = Unirest.get(this.getAuthUrl());
+        if (authenticationInfo != null && authenticationInfo.getUserName() != null) {
+            getRequest.basicAuth(authenticationInfo.getUserName(), authenticationInfo.getPassword());
+        }
+
+        HttpResponse<JsonNode> response = getRequest.asJson();
+        try (PrintWriter out = new PrintWriter(credentialsCachePath)) {
+            out.println(response.getBody().getObject().toString());
         }
     }
 }
